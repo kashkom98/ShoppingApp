511e97cf2c855d322615c3bc51c557ef21032919
kashkom98 <komandurisrinivasa.p@northeastern.edu>
2024-04-03 20:27:17 -0400

    order-service

--- order-service/src/main/java/com/shoppingapp/orderservice/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.shoppingapp.orderservice.repository;
-
-import com.shoppingapp.productservice.model.Product;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ProductRepository extends MongoRepository<Product, String> {
-}
