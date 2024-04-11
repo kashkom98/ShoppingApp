23c2052147273e0c8d2373108bec5b17c474c4f3
kashkom98 <komandurisrinivasa.p@northeastern.edu>
2024-04-11 17:00:27 -0400

    Added inventory service

--- inventory-service/src/main/java/com/shoppingapp/inventoryservice/model/Inventory.java
+++ inventory-service/src/main/java/com/shoppingapp/inventoryservice/model/Inventory.java
@@ -1,25 +1,23 @@
-package com.shoppingapp.orderservice.model;
+package com.shoppingapp.inventoryservice.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
 import jakarta.persistence.*;
-import java.util.List;
 
 @Entity
-@Table(name = "t_orders")
+@Table(name = "t_inventory")
 @Getter
 @Setter
-@NoArgsConstructor
 @AllArgsConstructor
-public class Order {
+@NoArgsConstructor
+public class Inventory {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String orderNumber;
-    @OneToMany(cascade = CascadeType.ALL)
-    private List<OrderLineItems> orderLineItemsList;
+    private String skuCode;
+    private Integer quantity;
 }
-
-
